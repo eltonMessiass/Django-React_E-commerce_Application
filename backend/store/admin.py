0ac3ca05a29fa8ffd688623eb29acb6e8f575bf3
eltonMessiass <eltonmessias10@gmail.com>
2024-05-12 23:15:25 +0200

    feat: all models done

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import *
-from .models import Product, Order, Category
+from .models import Product, Order, Category, OrderItem
 
-admin.site.register(Product)
+
 admin.site.register(Order)
+admin.site.register(OrderItem)
 admin.site.register(Category)
+admin.site.register(Product)
--- backend/store/models.py
+++ backend/store/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from users.models import Customer
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
 
 
 class Product(models.Model):
@@ -15,39 +15,34 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, blank=False)
     stock = models.PositiveIntegerField(null=False, blank=False, default=0)
     price = models.IntegerField(null=False, blank=False, default=0)
-    description = models.TextField(null=False, blank=False, default='')
+    description = models.TextField(null=True, blank=True, default='')
 
-    # image = models.ImageField(null=False, blank=False, default='default.jpg')
-
-    def __Str__(self):
+    def __str__(self):
         return self.name
 
-    @staticmethod
-    def get_products_by_id(ids):
-        return Product.objects.filter(id__in=ids)
-
-    @staticmethod
-    def get_all_products():
-        return Product.objects.all()
-
-    @staticmethod
-    def get_all_products_by_category_id(category_id):
-        if category_id:
-            return Product.objects.filter(category=category_id)
-        else:
-            return Product.get_all_products()
-
-
+    def calculate_total_price(self, quantity):
+        return quantity * self.price
 
 
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    address = models.CharField(max_length=50, default="", blank=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
     status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.username}"
 
     @staticmethod
     def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer=customer_id).order_by('-date')
+        return Order.objects.filter(customer=customer_id).order_by('-created_at')
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
