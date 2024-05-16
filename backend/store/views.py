23c41b09835474753ce12c874eda3db0480071de
eltonMessiass <eltonmessias10@gmail.com>
2024-05-16 02:55:03 +0200

    feat:used generics views for product and category

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,7 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
 from rest_framework import status, generics
-from rest_framework.decorators import api_view
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
 from rest_framework.response import Response
@@ -9,28 +7,20 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+
 class CategoryView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self, request):
-        try:
-            serializer = CategorySerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-
-class CategoryDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except:
-            raise Http404
 
     def get(self, request, pk):
         category = self.get_object(pk)
@@ -40,10 +30,43 @@ class CategoryDetailView(APIView):
     def delete(self, request, pk):
         category = self.get_object(pk)
         category.delete()
+      
+
     
 
+    
+class ProductListCreate(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+
+class ProductListByCategory(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category = self.kwargs['category']
+        products = Product.objects.filter(category=category)
+        return products
+
+
+
+        
 
 class ProductView(APIView):
+    def get_object(self,pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+        
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
@@ -55,25 +78,12 @@ class ProductView(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-
-    def get_products_by_category(self,request, category):
-        queryset = Product.objects.filter(category=category)
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
-class ProductDetailView(APIView):
-    def get_object(self,pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
-
+    
     def get(self, request, pk, format=None):
         product = self.get_object(pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
     def put(self, request, pk, format=None):
         product = self.get_object(pk)
         serializer = ProductSerializer(product, data=request.data)
@@ -81,7 +91,7 @@ class ProductDetailView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
     def delete(self, request, pk, format=None):
         product = self.get_object(pk)
         try:
@@ -89,6 +99,14 @@ class ProductDetailView(APIView):
             return Response(status=status.HTTP_200_OK)
         except Product.DoesNotExist:
             raise Http404
+
+
+
+
+
+    
+
+    
 
 
 
