7b307b41fac4224e50f307089d4a0be1c131f8ad
eltonMessiass <eltonmessias10@gmail.com>
2024-06-08 21:53:55 +0200

    feat:Protected route component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path='/' element={<ProductListing />} />
         <Route path='/products/:id/' element={<ProductDetail />} />
         <Route path='/cart' element={<Cart />} />
