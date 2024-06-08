dc9bdfe04dd28bf785c9282643f8470461d617e6
eltonMessiass <eltonmessias10@gmail.com>
2024-06-08 22:23:43 +0200

    feat:protectedRoute and LOgin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import ProductListing from './pages/ProductListing'
 import ProductDetail from './pages/ProductDetail'
 import './App.css'
 import Cart from './pages/Cart'
+import Login from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/login' element={<Login />} />
         
-        <Route path='/' element={<ProductListing />} />
+        <Route path='/' element={<ProtectedRoute><ProductListing /></ProtectedRoute>} />
         <Route path='/products/:id/' element={<ProductDetail />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
