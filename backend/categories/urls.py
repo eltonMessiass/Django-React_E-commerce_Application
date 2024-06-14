40e24d0b2d962737c7c52e58a535883f321d760d
eltonMessiass <eltonmessias10@gmail.com>
2024-06-15 00:57:16 +0200

    feat:categoty(update, create, delete)

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import CategoryListCreateView, CategoryDetailView
 
 
 urlpatterns = [
-    
+    path("category/", CategoryListCreateView.as_view(), name="book_create_List"),
+    path("category/<int:pk>/", CategoryDetailView.as_view(), name="category_detail")
 ]
--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+
+
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else: 
+            return (serializer.errors)
+        
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
