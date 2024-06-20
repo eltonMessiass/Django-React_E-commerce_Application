d8c6ac6585ce36ad7e79f1320320d6f4ade92045
eltonMessiass <eltonmessias10@gmail.com>
2024-06-20 08:12:27 +0200

    feat: model product created

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from categories.models import Category
 # Create your models here.
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    stock = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
