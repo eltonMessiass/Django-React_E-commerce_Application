1f83eb9b25df18c7d6e82e623a6ef6723af76c73
eltonMessiass <eltonmessias10@gmail.com>
2024-06-20 08:34:21 +0200

    feat: product list and create view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ProductPriceUpdateSerializer, ProductWriteSerializer, ProductReadSerializer
+from rest_framework import generics
+from .models import Product
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductWriteSerializer
+    permission_classes = [IsAuthenticated]
